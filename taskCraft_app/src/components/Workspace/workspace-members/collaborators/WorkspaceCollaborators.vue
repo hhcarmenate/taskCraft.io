47a9c48d9899185baface830698dcbeda68958d5
Henry Carmenate <henrycarmenateg@gmail.com>
2024-11-04 20:37:25 -0500

    Add guest view to WorkspaceCollaborators component
    Enable display of guest collaborators with filtering options within the WorkspaceCollaborators component. Updated the UI to show workspace members or guests based on the selected type, providing a "Leave" button for guest users.

--- taskCraft_app/src/components/Workspace/workspace-members/collaborators/WorkspaceCollaborators.vue
+++ taskCraft_app/src/components/Workspace/workspace-members/collaborators/WorkspaceCollaborators.vue
@@ -30,7 +30,7 @@ const selectType = (type) => {
         <span>(0/10)</span>
       </div>
     </div>
-    <div class="col-span-8">
+    <div class="col-span-8" v-if="typeSelected === 'workspace'">
       <div class="pb-6 text-sm border-b border-gray-500">
         <p>
           Workspace members can view and join all Workspace visible boards and create new boards in the Workspace.
@@ -79,7 +79,38 @@ const selectType = (type) => {
           </ul>
         </div>
       </div>
+    </div>
+    <div class="col-span-8" v-if="typeSelected === 'guest'">
+      <div class="pb-6 text-sm mt-5">
+        <h3 class="text-xl mb-2">Guests</h3>
+        <div class="mb-2">
+          <TextInput
+            placeholder="Filter by name"
+          />
+        </div>
+        <div class="flex flex-row gap-4 justify-between items-center">
+          <ul class="w-full">
+            <li>
+              <div class="border-y border-solid border-gray-500 flex flex-row justify-between items-center px-1 py-2">
+                <div class="member-name">
+                  Henry Carmenate
+                </div>
+                <div class="actions">
+                  <button
+                    class="px-2 ms-3 text-sm font-medium text-gray-900 focus:outline-none
+                    bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700
+                    focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700
+                    dark:bg-danger-800 dark:text-gray-400 dark:border-danger-800 dark:hover:text-white dark:hover:bg-danger-700"
+                  >
+                    Leave
+                  </button>
+                </div>
+              </div>
 
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
