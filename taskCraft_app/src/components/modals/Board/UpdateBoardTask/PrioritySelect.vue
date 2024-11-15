a9d857a3c17d5700179ed32d96213e1977312db9
Henry Carmenate <henrycarmenateg@gmail.com>
2024-11-15 15:50:34 -0500

    Simplify priority selection without form validation
    Removed the form validation for priority selection to streamline the user interface and reduce complexity in the code. Replaced the form-based select input with a standard HTML select dropdown to improve performance and simplify state management.

--- taskCraft_app/src/components/modals/Board/UpdateBoardTask/PrioritySelect.vue
+++ taskCraft_app/src/components/modals/Board/UpdateBoardTask/PrioritySelect.vue
@@ -1,9 +1,5 @@
 <script setup>
-import SelectInput from '@/components/fields/SelectInput.vue'
 import { computed, ref, watch } from 'vue'
-import { Form, useForm } from 'vee-validate'
-import { toTypedSchema } from '@vee-validate/zod'
-import * as zod from 'zod'
 
 // Constants
 const TASK_PRIORITIES = [
@@ -22,13 +18,7 @@ const props = defineProps({
 // Data
 const newPriority = ref(props.priority)
 const editingPriority = ref(false)
-const priorityForm = ref()
 
-const validationSchema = toTypedSchema(zod.object({
-  newPriority: zod.string().nonempty('Priority field is required!')
-}))
-
-const { handleSubmit } = useForm()
 
 // Computed properties
 const taskPriority = computed(() => {
@@ -46,22 +36,16 @@ const handleEditPriority = () => {
   editingPriority.value = true
 }
 
-// Handle form submission
-const onSubmit = handleSubmit(fields => {
-  console.log('Form submitted:', fields)
-})
+const handleFieldBlur = () => {
 
-// Handle invalid submission
-const onInvalidSubmit = (error) => {
-  console.log('Form submission error:', error)
 }
 
 // Watch for priority changes and trigger form submission
-watch(newPriority, () => {
-  if (priorityForm.value) {
-    priorityForm.value.submitForm().then(onSubmit).catch(onInvalidSubmit)
-  }
+watch(newPriority, (newValue) => {
+  console.log(newValue)
 })
+
+
 
 </script>
 
@@ -75,19 +59,22 @@ watch(newPriority, () => {
       {{ taskPriority }}
     </span>
     <span v-else>
-      <Form
-        ref="priorityForm"
-        :validation-schema="validationSchema"
-        @submit="onSubmit"
-        @invalid-submit="onInvalidSubmit"
+      <select
+        class="bg-gray-50 input-control mt-1 border border-gray-300 text-gray-900
+            text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block
+            w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
+            dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        v-model="newPriority"
+        @blur="handleFieldBlur"
       >
-        <SelectInput
-          name="newPriority"
-          :items="TASK_PRIORITIES"
-          v-model="newPriority"
-          show-error
-        />
-      </Form>
+        <option
+          v-for="item in TASK_PRIORITIES"
+          :value="item.value"
+          :key="item.value"
+        >
+          {{ item.text }}
+        </option>
+      </select>
     </span>
   </div>
 </template>
